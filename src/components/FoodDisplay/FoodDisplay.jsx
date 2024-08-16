87289c225e997d4f992df6659d8dbc093b281d02
AnilKumar3494 <karapaanilkumar005@gmail.com>
2024-08-16 08:40:27 -0400

    add:search bar

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,30 +1,39 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./FoodDisplay.css";
 import FoodItem from "../FoodItem/FoodItem";
 import { StoreContext } from "../../Context/StoreContext";
+import Searchbar from "../Search/Searchbar";
 
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Filter food_list based on category and search query
+  const filteredFoodList = food_list.filter((item) => {
+    return (
+      (category === "All" || category === item.food_category) &&
+      item.food_name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  });
 
   return (
     <div className="food-display" id="food-display">
       <h1>{`${category} Just For You`}</h1>
 
+      <Searchbar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+
       <div className="food-display-list">
-        {food_list.map((item) => {
-          if (category === "All" || category === item.food_category) {
-            return (
-              <FoodItem
-                key={item.food_id}
-                image={item.food_image}
-                name={item.food_name}
-                desc={item.food_desc}
-                price={item.food_price}
-                id={item.food_id}
-              />
-            );
-          }
-        })}
+        {filteredFoodList.map((item) => (
+          <FoodItem
+            key={item.food_id}
+            image={item.food_image}
+            name={item.food_name}
+            desc={item.food_desc}
+            price={item.food_price}
+            id={item.food_id}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,9 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer" id="footer">
-      <h3>Footer Will Be Here</h3>
+      <h2>Raindrop's</h2>
+      <a href="tel">+1 215-449-0040</a>
+      <a href="">2749 N 47th Street, Philadelphia, PA - 19104</a>
     </div>
   );
 };
