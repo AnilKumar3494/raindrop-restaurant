2ddbf03f3796644a048a51ed66006e112009cbf5
AnilKumar3494 <karapaanilkumar005@gmail.com>
2024-09-10 12:42:57 -0400

    minor updates

--- src/components/FoodItem/FoodItem.jsx
+++ src/components/FoodItem/FoodItem.jsx
@@ -4,6 +4,7 @@ import "./FoodItem.css";
 import { StoreContext } from "../../Context/StoreContext";
 import { assets } from "../../assets/assets";
 
+import { IoAddCircleOutline } from "react-icons/io5";
 // Work: Update done
 
 const FoodItem = ({ image, name, price, desc, id }) => {
@@ -16,35 +17,35 @@ const FoodItem = ({ image, name, price, desc, id }) => {
         <img className="food-item-image" src={image} alt={name} />
       </div>
 
-      {!cartItems[id] ? (
-        <img
-          className="add"
-          title="add to cart"
-          onClick={() => addToCart(id)}
-          src={assets.add_icon_white}
-          alt=""
-        />
-      ) : (
-        <div className="food-item-counter">
-          <img
-            src={assets.remove_icon_red}
-            onClick={() => removeFromCart(id)}
-            alt=""
-          />
-          <p className="food_items_count">{`${cartItems[id]}`}</p>
-          <img
-            src={assets.add_icon_green}
-            onClick={() => addToCart(id)}
-            alt=""
-          />
-        </div>
-      )}
-
       <div className="food-item-info">
         <h3 className="food_name">{name}</h3>
         <p className="food-item-desc">{desc}</p>
         <div className="price_add">
-          <h3>${price}</h3>
+          <h3 className="price">${price}</h3>
+          {!cartItems[id] ? (
+            <div className="add_container">
+              <IoAddCircleOutline
+                className="add"
+                title="add to cart"
+                onClick={() => addToCart(id)}
+                src={assets.add_icon_white}
+              />
+            </div>
+          ) : (
+            <div className="food-item-counter">
+              <img
+                src={assets.remove_icon_red}
+                onClick={() => removeFromCart(id)}
+                alt=""
+              />
+              <p className="food_items_count">{`${cartItems[id]}`}</p>
+              <img
+                src={assets.add_icon_green}
+                onClick={() => addToCart(id)}
+                alt=""
+              />
+            </div>
+          )}
         </div>
       </div>
     </div>
